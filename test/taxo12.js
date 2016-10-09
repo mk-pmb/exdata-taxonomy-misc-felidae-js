850aab3ed52aefddbdf24402f3ec01862ad06304
mk-pmb <mk-pmb@users.noreply.github.com>
2016-10-09 22:55:35 +0200

    v0.1.1: clarify test

--- test/taxo12.js
+++ test/taxo12.js
@@ -2,11 +2,12 @@
 /* -*- tab-width: 2 -*- */
 'use strict';
 
-var eq = require('assert').deepStrictEqual, taxo1, taxo2, path,
+var eq = require('assert').deepStrictEqual, taxo1, taxo2, paths,
   exdataFelidae = require('exdata-taxonomy-misc-felidae');
 
-path = exdataFelidae.findPaths('oldid', 742216395);
-eq(path, [ ['Felinae', 'Lynx', 'rufus', 'wp-en'] ]);
+paths = exdataFelidae.findPaths('oldid', 742216395);
+eq(paths.length, 1);
+eq(paths[0], ['Felinae', 'Lynx', 'rufus', 'wp-en']);
 
 taxo1 = exdataFelidae.taxo();
 taxo1.Felinae.Lynx.rufus.commonName = 'bobcat';
